CarbonROM/android_vendor_carbon

sdclang: Gaurd usage
Change-Id: Ie3e1c8130a7b10e2a721c66fe247a4cf6e8373a8
Signed-off-by: Joe Maples <joe@frap129.org>

config/common.mk
@@ -91,5 +91,9 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/carbon/overlay/common
 # Squisher Location
 SQUISHER_SCRIPT := vendor/carbon/tools/squisher
 
-# include definitions for SDCLANG
-include device/qcom/common/sdclang/sdclang.mk
+# Include SDCLANG definitions if it is requested and available
+ifeq ($(HOST_OS),linux)
+    ifneq ($(wildcard vendor/qcom/sdclang-3.8/),)
+        include vendor/carbon/sdclang/sdclang.mk
+    endif
+endif

sdclang/sdclang.mk
@@ -17,6 +17,6 @@ SDCLANG_FORCED ?= false
 
 SDCLANG_PATH := vendor/qcom/sdclang-3.8/linux-x86/bin
 
-SDCLANG_LTO_DEFS := device/qcom/common/sdclang/sdllvm-lto-defs.mk
+SDCLANG_LTO_DEFS := vendor/carbon/sdclang/sdllvm-lto-defs.mk
 
 SDCLANG_COMMON_FLAGS := -O3 -fvectorize-loops

AquariOS/vendor_aquarios

config/common.mk
@@ -233,5 +233,9 @@ endif
 
 $(call prepend-product-if-exists, vendor/extra/product.mk)
 
-# include definitions for SDCLANG
-include device/qcom/common/sdclang/sdclang.mk
+# Include SDCLANG definitions if it is requested and available
+ifeq ($(HOST_OS),linux)
+    ifneq ($(wildcard vendor/qcom/sdclang-3.8/),)
+        include vendor/aquarios/sdclang/sdclang.mk
+    endif
+endif

sdclang/sdclang.mk
@@ -17,6 +17,6 @@ SDCLANG_FORCED ?= false
 
 SDCLANG_PATH := vendor/qcom/sdclang-3.8/linux-x86/bin
 
-SDCLANG_LTO_DEFS := device/qcom/common/sdclang/sdllvm-lto-defs.mk
+SDCLANG_LTO_DEFS := vendor/aquarios/sdclang/sdllvm-lto-defs.mk
 
 SDCLANG_COMMON_FLAGS := -O3 -fvectorize-loops