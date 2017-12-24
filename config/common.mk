InvictrixROM-Mirror/vendor_invictus

Let's get Unlimited Google Photos

config/common.mk
@@ -63,6 +63,10 @@ PRODUCT_COPY_FILES += \
     vendor/invictus/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit \
     vendor/invictus/prebuilt/common/bin/sysinit:system/bin/sysinit
 
+# Unlimited Google Photos HQ upload
+PRODUCT_COPY_FILES += \
+   vendor/invictus/prebuilt/common/etc/sysconfig/nexus.xml:system/etc/sysconfig/nexus.xml
+
 # Required packages
 PRODUCT_PACKAGES += \
     CellBroadcastReceiver \

AquariOS/vendor_aquarios

Change-Id: I2e99f77b7a16b64ed79a56becc25cd90b394863f

config/common.mk
@@ -63,6 +63,10 @@ PRODUCT_COPY_FILES += \
     vendor/aquarios/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit \
     vendor/aquarios/prebuilt/common/bin/sysinit:system/bin/sysinit
 
+# Unlimited Google Photos HQ upload
+PRODUCT_COPY_FILES += \
+   vendor/aquarios/prebuilt/common/etc/sysconfig/nexus.xml:system/etc/sysconfig/nexus.xml
+
 # Required packages
 PRODUCT_PACKAGES += \
     CellBroadcastReceiver \