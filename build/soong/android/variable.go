620b32b9dd9d1a8821dbb9cc3c41679b8f2dd9c3
AnilKumar Chimata <anilc@codeaurora.org>
2018-06-20 20:25:55 +0530

    soong_config: Add new flags for HW FDE
    Add new flags for HW FDE to pass CFlags to source code.
    
    CRs-Fixed: 2210986
    Change-Id: I70d68f015ee8eb8d123ce863ce8e36d0310bc082

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -6,10 +6,20 @@ type Product_variables struct {
 	}
 	Recovery_skip_ev_rel_input struct {
 		Cflags []string
+
+	Supports_hw_fde struct {
+		Cflags []string
+		Header_libs []string
+		Shared_libs []string
+	}
+	Supports_hw_fde_perf struct {
+		Cflags []string
 	}
 }
 
 type ProductVariables struct {
 	Bootloader_message_offset  *int `json:",omitempty"`
 	Recovery_skip_ev_rel_input  *bool `json:",omitempty"`
+	Supports_hw_fde  *bool `json:",omitempty"`
+	Supports_hw_fde_perf  *bool `json:",omitempty"`
 }
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -6,6 +6,8 @@ _json_contents := $(_json_contents)     "Aquarios":{$(newline)
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
 $(call add_json_val_default, Bootloader_message_offset, $(BOOTLOADER_MESSAGE_OFFSET), 0)
 $(call add_json_bool, Recovery_skip_ev_rel_input, $(filter true,$(TARGET_RECOVERY_SKIP_EV_REL_INPUT)))
+$(call add_json_bool, Supports_hw_fde, $(filter true,$(TARGET_HW_DISK_ENCRYPTION)))
+$(call add_json_bool, Supports_hw_fde_perf, $(filter true,$(TARGET_HW_DISK_ENCRYPTION_PERF)))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
 _json_contents := $(_json_contents)__SV_END
