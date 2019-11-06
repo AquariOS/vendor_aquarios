premaca/vendor_gzosp

Revert "[TEMP] Revert "gzosp: create privapp-permissions-gzr""
This reverts commit ab04a4ae1b189c6eb6b732e508c58cf7af36a798.

Change-Id: Id5c4a288f94fc3332cb390f3d37a1d9306ea7a24

config/common.mk
@@ -60,6 +60,10 @@ PRODUCT_COPY_FILES += \
 #PRODUCT_COPY_FILES +=  \
 #    vendor/gzosp/prebuilt/common/sysconfig/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml
 
+# privapp permissions
+PRODUCT_COPY_FILES += \
+    vendor/gzosp/prebuilt/common/etc/permissions/privapp-permissions-gzr.xml:system/etc/permissions/privapp-permissions-gzr.xml
+
 # Gzosp-specific startup services
 PRODUCT_COPY_FILES += \
     vendor/gzosp/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \

AquariOS/vendor_aquarios

config/common.mk
@@ -60,6 +60,10 @@ PRODUCT_COPY_FILES += \
 #PRODUCT_COPY_FILES +=  \
 #    vendor/aquarios/prebuilt/common/sysconfig/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml
 
+# privapp permissions
+PRODUCT_COPY_FILES += \
+    vendor/aquarios/prebuilt/common/etc/permissions/privapp-permissions-aqua.xml:system/etc/permissions/privapp-permissions-aqua.xml
+
 # AquariOS-specific startup services
 PRODUCT_COPY_FILES += \
     vendor/aquarios/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \