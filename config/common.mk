sparxuk/vendor_validus

permissions: bring back google perms from 9.0
Change-Id: I954e57191308173b5f7ca3a4cf7c143b6e20354e

config/common.mk
@@ -62,7 +62,8 @@ PRODUCT_COPY_FILES += \
 
 # privapp permissions
 PRODUCT_COPY_FILES += \
-    vendor/validus/prebuilt/common/etc/permissions/privapp-permissions-gzr.xml:system/etc/permissions/privapp-permissions-gzr.xml
+    vendor/validus/prebuilt/common/etc/permissions/privapp-permissions-gzr.xml:system/etc/permissions/privapp-permissions-gzr.xml \
+    vendor/validus/prebuilt/common/etc/permissions/privapp-permissions-google.xml:system/etc/permissions/privapp-permissions-google.xml
 
 # Validus-specific startup services
 PRODUCT_COPY_FILES += \

AquariOS/vendor_aquarios

config/common.mk
@@ -62,7 +62,8 @@ PRODUCT_COPY_FILES += \
 
 # privapp permissions
 PRODUCT_COPY_FILES += \
-    vendor/aquarios/prebuilt/common/etc/permissions/privapp-permissions-aqua.xml:system/etc/permissions/privapp-permissions-aqua.xml
+    vendor/aquarios/prebuilt/common/etc/permissions/privapp-permissions-aqua.xml:system/etc/permissions/privapp-permissions-aqua.xml \
+    vendor/aquarios/prebuilt/common/etc/permissions/privapp-permissions-google.xml:system/etc/permissions/privapp-permissions-google.xml
 
 # AquariOS-specific startup services
 PRODUCT_COPY_FILES += \