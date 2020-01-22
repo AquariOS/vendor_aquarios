9e1b3d6bec37118b885a07c2bfd4bbe63e84a9a2
calebcabob <calphonic@gmail.com>
2020-01-22 11:23:15 -0500

    vendor: Add flame, taimen, and walleye to the list
    Change-Id: Idcc53091dc00f6c345ab41cc27603d906674b01e

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,15 @@
 . "$(gettop)/vendor/extras/build/envsetup.sh"
 
-aosp_devices=('blueline' 'bonito' 'crosshatch' 'marlin' 'coral')
+aosp_devices=(
+'blueline'
+'bonito'
+'coral'
+'crosshatch'
+'flame'
+'marlin'
+'taimen'
+'walleye'
+)
 
 function lunch_devices() {
     add_lunch_combo aqua_${device}-user
