1de1b599396c535e3668f8bbce7c97d093eb4eb4
calebcabob <calphonic@gmail.com>
2020-02-17 10:54:45 -0500

    vendorsetup: Remove eng option from lunch menu (cleaner view again)
    - There's no need for this when we can simply just:
    
    	`breakfast aqua_<device>-eng`
    
    Change-Id: I076619902dbfa28bebee9db5d80a114cddd089fe

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,7 +14,6 @@ aosp_devices=(
 function lunch_devices() {
     add_lunch_combo aqua_${device}-user
     add_lunch_combo aqua_${device}-userdebug
-    add_lunch_combo aqua_${device}-eng
 }
 
 for device in ${aosp_devices[@]}; do
