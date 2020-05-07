ad0ac001e31fcba8dd0d642a65bd8d94564b0ffa
Paul Keith <javelinanddart@gmail.com>
2020-05-06 19:10:03 -0700

    AquariOS: Add flag for skipping EV_REL input in recovery
    * Needed now that recovery is in bp
    
    Change-Id: I625ac22e79912cbdea328eba12dc1dd16448e7a8

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,11 +1,15 @@
 package android
 
-type ProductVariables struct {
+type Product_variables struct {
 	Bootloader_message_offset struct {
+		Cflags []string
+	}
+	Recovery_skip_ev_rel_input struct {
 		Cflags []string
 	}
 }
 
 type ProductVariables struct {
 	Bootloader_message_offset  *int `json:",omitempty"`
+	Recovery_skip_ev_rel_input  *bool `json:",omitempty"`
 }
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,11 +1,13 @@
+add_json_str_omitempty = $(if $(strip $(2)),$(call add_json_str, $(1), $(2)))
 add_json_val_default = $(call add_json_val, $(1), $(if $(strip $(2)), $(2), $(3)))
 
-_contents := $(_contents)    "Aquarios":{$(newline)
+_json_contents := $(_json_contents)     "Aquarios":{$(newline)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
 $(call add_json_val_default, Bootloader_message_offset, $(BOOTLOADER_MESSAGE_OFFSET), 0)
+$(call add_json_bool, Recovery_skip_ev_rel_input, $(filter true,$(TARGET_RECOVERY_SKIP_EV_REL_INPUT)))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
-_contents := $(_contents)__SV_END
+_json_contents := $(_json_contents)__SV_END
 
-_contents := $(_contents)    },$(newline)
+_json_contents := $(_json_contents)    },$(newline)
