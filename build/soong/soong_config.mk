8ca4e3c1861d4fd730a8929cd72ae1d8a4a1e169
Paul Keith <javelinanddart@gmail.com>
2020-05-06 19:10:03 -0700

    AquariOS: Add flag for skipping EV_REL input in recovery
    * Needed now that recovery is in bp
    
    Change-Id: I625ac22e79912cbdea328eba12dc1dd16448e7a8

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -4,6 +4,7 @@ _contents := $(_contents)    "Aquarios":{$(newline)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
 $(call add_json_val_default, Bootloader_message_offset, $(BOOTLOADER_MESSAGE_OFFSET), 0)
+$(call add_json_bool, Recovery_skip_ev_rel_input, $(filter true,$(TARGET_RECOVERY_SKIP_EV_REL_INPUT)))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
 _contents := $(_contents)__SV_END
