2fc9bbfffa48317cf4780adf7353a4e1e705d63b
Paul Keith <javelinanddart@gmail.com>
2020-05-06 19:10:03 -0700

    AquariOS: Add flag for skipping EV_REL input in recovery
    * Needed now that recovery is in bp
    
    Change-Id: I625ac22e79912cbdea328eba12dc1dd16448e7a8

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -4,8 +4,12 @@ type Product_variables struct {
 	Bootloader_message_offset struct {
 		Cflags []string
 	}
+	Recovery_skip_ev_rel_input struct {
+		Cflags []string
+	}
 }
 
 type ProductVariables struct {
 	Bootloader_message_offset  *int `json:",omitempty"`
+	Recovery_skip_ev_rel_input  *bool `json:",omitempty"`
 }
